e14eca7981e8bcf0b3aaa583f1a1dd168da5dfd3
aahladmadireddy <aahladmn99@gmail.com>
2017-07-27 00:00:35 -0500

    spelling error to word ratio is not working and is in output

--- process.py
+++ process.py
@@ -2,9 +2,10 @@ import spacy
 import os
 import json
 from textstat.textstat import textstat
+import enchant
 
 nlp = spacy.load('en')
-
+dict = enchant.Dict('en_US')
 reviews = []
 processedOutput = []
 
@@ -20,6 +21,7 @@ with open('generated/filteredTrainingData.json', 'r') as data:
 
         numberOfSentences = 0
         numberOfWords = 0
+        numberOfSpellingErrors = 0.0
 
         for sentence in processed.sents:
             numberOfSentences += 1
@@ -28,18 +30,24 @@ with open('generated/filteredTrainingData.json', 'r') as data:
         for token in processed:
             if token.is_punct is False:
                 numberOfWords += 1
+
+            if dict.check(token) is False and token.is_punct is False and str(token) != "$":
+                numberOfSpellingErrors += 1.0
+
+        spellingErrorToWordRatio = numberOfSpellingErrors / numberOfWords
+
         output.append(numberOfWords)
 
-        numberOfSyllables = textstat.syllable_count(reviewText)
-
-        wordsPerSentence = numberOfWords / numberOfSentences
-        syllablesPerWord = numberOfSyllables / numberOfWords
-
+        # numberOfSyllables = textstat.syllable_count(reviewText)
+        # wordsPerSentence = numberOfWords / numberOfSentences
+        # syllablesPerWord = numberOfSyllables / numberOfWords
         # readingScore = (0.39 * wordsPerSentence) + (11.8 * syllablesPerWord) - 15.59
 
         readingScore = textstat.flesch_kincaid_grade(reviewText)
 
         output.append(readingScore)
+
+        output.append(spellingErrorToWordRatio)
 
         print (output)
 
